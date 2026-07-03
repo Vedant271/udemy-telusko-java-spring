5cd0c0c2e6859fd9f416e499465e6480f322d71a
Vedant Karale <vedantkarale271@gmail.com>
2026-07-04 00:53:46 +0530

    163. Spring 1st Project

---
+++ exploring_spring_framework/src/main/java/org/telusko/Alien.java
@@ -0,0 +1,7 @@
+package org.telusko;
+
+public class Alien {
+    public void Code() {
+        System.out.println("I am an Coding");
+    }
+}
---
+++ exploring_spring_framework/src/main/java/org/telusko/App.java
@@ -0,0 +1,14 @@
+package org.telusko;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        Alien obj = (Alien) context.getBean("alien");
+        obj.Code();
+    }
+}
