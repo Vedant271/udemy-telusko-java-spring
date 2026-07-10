e78ebf43611e911f329d4b9ddb74f3bbbda6510e
Vedant Karale <vedantkarale271@gmail.com>
2026-07-10 20:38:40 +0530

    127. Fetching all Records

--- jdbc_java_database_connectivity/src/JDBCDemo.java
+++ jdbc_java_database_connectivity/src/JDBCDemo.java
@@ -5,16 +5,24 @@ public class JDBCDemo {
         String url = "jdbc:postgresql://localhost:5433/demo";
         String username = "postgres";
         String password = "Vedant@271";
-        String query = "Select sname from student where sid = 1";
+        String query1 = "Select sname from student where sid = 1";
+        String query2 = "Select * from student";
 
         try{
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connection Established");
 
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-            resultSet.next();
-            System.out.println("Student from database is " + resultSet.getString("sname"));
+//            ResultSet resultSet = statement.executeQuery(query1);
+//            resultSet.next();
+//            System.out.println("Student from database is " + resultSet.getString("sname"));
+
+            ResultSet resultSet = statement.executeQuery(query2);
+            while(resultSet.next()) {
+                System.out.print(resultSet.getInt(1) + " - ");
+                System.out.print(resultSet.getString(2) + " - ");
+                System.out.println(resultSet.getInt(3));
+            }
 
             connection.close();
             System.out.println("connection Closed");
