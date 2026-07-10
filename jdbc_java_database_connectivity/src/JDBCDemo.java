6a5e76d27df1f7bc119dae2389a92a3600bf6965
Vedant Karale <vedantkarale271@gmail.com>
2026-07-10 20:34:47 +0530

    126. Execute and Proecess

--- jdbc_java_database_connectivity/src/JDBCDemo.java
+++ jdbc_java_database_connectivity/src/JDBCDemo.java
@@ -1,17 +1,23 @@
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBCDemo {
     public static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:5433/demo";
         String username = "postgres";
         String password = "Vedant@271";
+        String query = "Select sname from student where sid = 1";
 
         try{
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connection Established");
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            resultSet.next();
+            System.out.println("Student from database is " + resultSet.getString("sname"));
+
+            connection.close();
+            System.out.println("connection Closed");
         }catch(SQLException e){
             System.out.println(e.getMessage());
         }
