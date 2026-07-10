3f2694ed00cca64d7314cd8365918e60b143bfff
Vedant Karale <vedantkarale271@gmail.com>
2026-07-10 20:53:14 +0530

    128. Crud Operations - Insert

---
+++ jdbc_java_database_connectivity/src/JDBCDemo2.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+
+public class JDBCDemo2 {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5433/demo";
+        String username = "postgres";
+        String password = "Vedant@271";
+        String query1 = "Insert into student values(6, 'John', 85)";
+        String query2 = "Update student set name = 'Max' where sid = 5";
+        String query3 = "Delete from student where sid = 5";
+
+        try{
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection Established");
+
+            Statement statement = connection.createStatement();
+            // execute can hit both Select as well as Insert/Update/Delete
+            // execute returns true if ResultSet is returned i.e. Select query is hit
+            // execute returns false if ResultSet is not returned i.e. Insert/Update/Delete query is hit
+            boolean status = statement.execute(query1);
+            System.out.println(status);
+
+            connection.close();
+            System.out.println("connection Closed");
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
